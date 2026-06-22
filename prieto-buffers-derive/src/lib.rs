43ec44abaa815e6dbf887443187d864b6d93f51d
Jordise2002 <jordisegura2002@gmail.com>
2026-06-22 12:07:28 +0200

    innitail commit

---
+++ prieto-buffers-derive/src/lib.rs
@@ -0,0 +1,179 @@
+use proc_macro::TokenStream;
+use quote::quote;
+
+use syn::{Meta, Lit};
+
+fn parse_u8_from_attr(attr: &syn::Attribute) -> Option<u8> {
+    match &attr.meta {
+        Meta::List(meta_list) => {
+            if let Some(_first) = meta_list.tokens.clone().into_iter().next() {
+                let lit: Lit = syn::parse2(meta_list.tokens.clone()).ok()?;
+                if let Lit::Int(lit_int) = lit {
+                    if let Ok(value) = lit_int.base10_parse::<u8>() {
+                        if value > 31 {
+                            panic!("Field ID must be between 0 and 31");
+                        }
+
+                        return Some(value);
+                    }
+                }
+            }
+            None
+        }
+        _ => None,
+    }
+}
+
+use std::collections::HashSet;
+
+fn generate_non_defined_ids(v: Vec<Option<u8>>) -> Option<Vec<u8>> {
+    let mut used = HashSet::new();
+
+    // recoger los usados
+    for x in v.iter().flatten() {
+        used.insert(*x);
+    }
+
+    let mut result = Vec::with_capacity(v.len());
+    let mut next_id: u8 = 0;
+
+    for slot in v {
+        match slot {
+            Some(x) => {
+                used.insert(x);
+                result.push(x);
+            }
+            None => {
+                while next_id <= 31 && used.contains(&next_id) {
+                    if next_id == 31 {
+                        return None;
+                    }
+                    next_id += 1;
+                }
+
+                if used.contains(&next_id) {
+                    return None;
+                }
+
+                used.insert(next_id);
+                result.push(next_id);
+            }
+        }
+    }
+
+    Some(result)
+}
+
+#[proc_macro_derive(PrietoBuffersSerde, attributes(field_id))]
+pub fn derive_prieto_buffer_serde(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+
+    let struct_name = &input.ident;
+
+    let fields = match &input.data {
+        syn::Data::Struct(data) => &data.fields,
+        _ => panic!("PrietoBuffersSerde can only be derived for structs"),
+    };
+
+    let field_names: Vec<_>= fields.iter().map(|field| {
+        field.ident.as_ref().expect("Fields must be named")
+    }).collect();
+
+    let field_ammount = field_names.len();
+
+    let defined_field_ids = fields.iter().map(|field| {
+        for attr in &field.attrs {
+            if attr.path().is_ident("field_id") {
+                return parse_u8_from_attr(attr);
+            }
+        }
+
+        return None
+    }).collect::<Vec<_>>();
+
+
+    let field_ids = generate_non_defined_ids(defined_field_ids).expect("Too many fields, not enough IDs available");
+
+    quote! {
+        impl #struct_name {
+            pub fn skip_field(bytes: &[u8], field_type: prieto_buffers::FieldType) -> u32 {
+                if let prieto_buffers::FieldType::Struct = field_type {
+                    let field_count = bytes[0] as u32;
+                    let mut offset = 1;
+
+                    for _ in 0..field_count {
+                        let field_header = bytes[offset as usize];
+                        offset += 1;
+
+                        let field_type = prieto_buffers::FieldType::from_u8(field_header >> 5).unwrap();
+                        offset += #struct_name::skip_field(&bytes[offset as usize..], field_type);
+                    }
+
+                    offset as u32
+                }
+                else {
+                    field_type.get_size() as u32
+                }
+            }
+        }
+
+        impl PrietoBuffersSerde for #struct_name {
+            fn get_size(&self) -> u32 {
+                let mut size = 1;
+                #(size += self.#field_names.get_size() + 1;)*
+                size
+            }
+
+            fn get_type(&self) -> prieto_buffers::FieldType {
+                prieto_buffers::FieldType::Struct
+            }
+
+            fn serialize(&self, bytes: &mut [u8]) {
+                let mut offset:u32 = 0;
+    
+                bytes[offset as usize] = #field_ammount as u8;
+                offset += 1;
+
+                #(
+                    self.#field_names.serialize_with_header(#field_ids, &mut bytes[offset as usize..]);
+                    offset += self.#field_names.get_size() + 1;
+                )*
+            }
+
+            fn deserialize(&mut self, bytes: &[u8]) {
+                let mut offset:u32 = 0;
+                let mut counter:u8 = 0;
+                
+                let field_count = bytes[offset as usize];
+                offset += 1;
+
+                for _ in 0..field_count {
+                    let field_header = bytes[offset as usize];
+                    offset += 1;
+
+                    let field_id = field_header & 0b00011111;
+                    let field_type = prieto_buffers::FieldType::from_u8(field_header >> 5).unwrap();
+
+                    let field_size = match field_id {
+                        #(
+                            #field_ids => {
+                                if self.#field_names.get_type() == field_type {
+                                    self.#field_names.deserialize(&bytes[offset as usize..]);
+                                    self.#field_names.get_size()
+                                }
+                                else {
+                                    #struct_name::skip_field(&bytes[offset as usize..], field_type)
+                                }
+                            }
+                        )*
+                        _ => {
+                            #struct_name::skip_field(&bytes[offset as usize..], field_type)
+                        }
+                    };
+
+                    offset += field_size;
+                }
+            }
+        }
+    }.into()
+}
---
+++ prieto-buffers/src/lib.rs
@@ -0,0 +1,249 @@
+#![no_std]
+
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum FieldType {
+    SingleByte = 0,
+    TwoBytes = 1,
+    FourBytes = 2,
+    EightBytes = 3,
+    Struct = 4
+}
+
+impl FieldType {
+    pub fn get_size(&self) -> usize {
+        match self {
+            FieldType::SingleByte => 1,
+            FieldType::TwoBytes => 2,
+            FieldType::FourBytes => 4,
+            FieldType::EightBytes => 8,
+            FieldType::Struct => 0// Struct sizes are dynamic and determined by their fields
+        }
+    }
+
+    pub fn from_u8(value: u8) -> Option<FieldType> {
+        match value {
+            0 => Some(FieldType::SingleByte),
+            1 => Some(FieldType::TwoBytes),
+            2 => Some(FieldType::FourBytes),
+            3 => Some(FieldType::EightBytes),
+            4 => Some(FieldType::Struct),
+            _ => None,
+        }
+    }
+}
+
+fn build_field_header(field_id: u8, field_type: FieldType) -> u8 {
+    if field_id > 31 {
+        panic!("Field ID must be between 0 and 31");
+    }
+
+    let field_type_bits = (field_type as u8) << 5;
+    let field_id_bits = field_id;
+
+    field_type_bits | field_id_bits
+}
+
+#[cfg(feature = "derive")]
+pub use prieto_buffers_derive::PrietoBuffersSerde;
+
+pub trait PrietoBuffersSerde {
+    fn get_size(&self) -> u32;
+    fn get_type(&self) -> FieldType;
+    fn serialize(&self, bytes: &mut [u8]);
+    
+    fn serialize_with_header(&self, field_id: u8, bytes: &mut [u8])
+    {
+        bytes[0] = build_field_header(field_id, self.get_type());
+        self.serialize(& mut bytes[1..]);
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]);
+}
+
+impl PrietoBuffersSerde for u8 {
+    fn get_size(&self) -> u32 {
+        1
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::SingleByte
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        bytes[0] = *self;
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        *self = bytes[0];
+    }
+}
+
+impl PrietoBuffersSerde for i8 {
+    fn get_size(&self) -> u32 {
+        1
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::SingleByte
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        bytes[0] = *self as u8;
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        *self = bytes[0] as i8;
+    }
+}
+
+impl PrietoBuffersSerde for bool {
+    fn get_size(&self) -> u32 {
+        1
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::SingleByte
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        bytes[0] = if *self { 1 } else { 0 };
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        *self = bytes[0] != 0;
+    }
+}
+
+impl PrietoBuffersSerde for u16 {
+    fn get_size(&self) -> u32 {
+        2
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::TwoBytes
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        bytes[0] = (*self & 0xFF) as u8;
+        bytes[1] = ((*self >> 8) & 0xFF) as u8;
+    }
+
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        *self = (bytes[0] as u16) | ((bytes[1] as u16) << 8);
+    }
+}
+
+impl PrietoBuffersSerde for i16 {
+    fn get_size(&self) -> u32 {
+        2
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::TwoBytes
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        bytes[0] = (*self & 0xFF) as u8;
+        bytes[1] = ((*self >> 8) & 0xFF) as u8;
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        *self = (bytes[0] as i16) | ((bytes[1] as i16) << 8);
+    }
+}
+
+impl PrietoBuffersSerde for u32 {
+    fn get_size(&self) -> u32 {
+        4
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::FourBytes
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        bytes[0] = (*self & 0xFF) as u8;
+        bytes[1] = ((*self >> 8) & 0xFF) as u8;
+        bytes[2] = ((*self >> 16) & 0xFF) as u8;
+        bytes[3] = ((*self >> 24) & 0xFF) as u8;
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        *self = (bytes[0] as u32)
+            | ((bytes[1] as u32) << 8)
+            | ((bytes[2] as u32) << 16)
+            | ((bytes[3] as u32) << 24);
+    }
+}
+
+impl PrietoBuffersSerde for i32 {
+    fn get_size(&self) -> u32 {
+        4
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::FourBytes
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        bytes[0] = (*self & 0xFF) as u8;
+        bytes[1] = ((*self >> 8) & 0xFF) as u8;
+        bytes[2] = ((*self >> 16) & 0xFF) as u8;
+        bytes[3] = ((*self >> 24) & 0xFF) as u8;
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        *self = (bytes[0] as i32)
+            | ((bytes[1] as i32) << 8)
+            | ((bytes[2] as i32) << 16)
+            | ((bytes[3] as i32) << 24);
+    }
+}
+
+impl PrietoBuffersSerde for u64 {
+    fn get_size(&self) -> u32 {
+        8
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::EightBytes
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        for i in 0..8 {
+            bytes[i] = ((*self >> (i * 8)) & 0xFF) as u8;
+        }
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        *self = 0;
+        for i in 0..8 {
+            *self |= (bytes[i] as u64) << (i * 8);
+        }
+    }
+}
+
+impl PrietoBuffersSerde for i64 {
+    fn get_size(&self) -> u32 {
+        8
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::EightBytes
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        for i in 0..8 {
+            bytes[i] = ((*self >> (i * 8)) & 0xFF) as u8;
+        }
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        *self = 0;
+        for i in 0..8 {
+            *self |= (bytes[i] as i64) << (i * 8);
+        }
+    }
+}
