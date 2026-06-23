bbec4cef479b85c4eeec9460d2ba601819128435
Jordi <jordisegura2002@gmail.com>
2026-06-23 13:25:47 +0200

    added tests

---
+++ test-suite/src/test/header.rs
@@ -0,0 +1,152 @@
+#[test]
+fn test_header_datatype() {
+    use prieto_buffers::PrietoBuffersSerde;
+
+    fn extract_datatype_from_header(header: u8) -> Option<prieto_buffers::FieldType> {
+        let data_type = header >> 5;
+        prieto_buffers::FieldType::from_u8(data_type)
+    }
+
+    let a: u8 = 1;
+
+    let mut a_buffer = [0; 2];
+    a.serialize_with_header(0, a_buffer.as_mut_slice());
+
+    assert_eq!(
+        prieto_buffers::FieldType::SingleByte,
+        extract_datatype_from_header(a_buffer[0]).expect("Invalid data type header")
+    );
+
+    let b: i8 = -1;
+    let mut b_buffer = [0; 2];
+    b.serialize_with_header(1, b_buffer.as_mut_slice());
+
+    assert_eq!(
+        prieto_buffers::FieldType::SingleByte,
+        extract_datatype_from_header(b_buffer[0]).expect("Invalid data type header")
+    );
+
+    let c: u16 = 2;
+
+    let mut c_buffer = [0; 3];
+    c.serialize_with_header(2, c_buffer.as_mut_slice());
+
+    assert_eq!(
+        prieto_buffers::FieldType::TwoBytes,
+        extract_datatype_from_header(c_buffer[0]).expect("Invalid data type header")
+    );
+
+    let d: i16 = -2;
+
+    let mut d_buffer = [0; 3];
+    d.serialize_with_header(3, d_buffer.as_mut_slice());
+
+    assert_eq!(
+        prieto_buffers::FieldType::TwoBytes,
+        extract_datatype_from_header(d_buffer[0]).expect("Invalid data type header")
+    );
+
+    let e: u32 = 3;
+
+    let mut e_buffer = [0; 5];
+    e.serialize_with_header(4, e_buffer.as_mut_slice());
+
+    assert_eq!(
+        prieto_buffers::FieldType::FourBytes,
+        extract_datatype_from_header(e_buffer[0]).expect("Invalid data type")
+    );
+
+    let f: i32 = -3;
+
+    let mut f_buffer = [0; 5];
+    f.serialize_with_header(5, f_buffer.as_mut_slice());
+
+    assert_eq!(
+        prieto_buffers::FieldType::FourBytes,
+        extract_datatype_from_header(f_buffer[0]).expect("Invalid data type header")
+    );
+
+    let g: u64 = 4;
+
+    let mut g_buffer = [0; 9];
+    g.serialize_with_header(6, g_buffer.as_mut_slice());
+
+    assert_eq!(
+        prieto_buffers::FieldType::EightBytes,
+        extract_datatype_from_header(g_buffer[0]).expect("Invalid data type header")
+    );
+
+    let h: i64 = -4;
+
+    let mut h_buffer = [0; 9];
+    h.serialize_with_header(7, h_buffer.as_mut_slice());
+
+    assert_eq!(
+        prieto_buffers::FieldType::EightBytes,
+        extract_datatype_from_header(h_buffer[0]).expect("Invalid data type header")
+    );
+}
+
+#[test]
+fn test_header_field_id() {
+    use prieto_buffers::PrietoBuffersSerde;
+    
+    fn extract_field_id_from_header(header: u8) -> u8 {
+        header & 0b00011111
+    }
+    
+    let a: u8 = 1;
+
+    let mut a_buffer = [0; 2];
+    a.serialize_with_header(0, a_buffer.as_mut_slice());
+
+    assert_eq!(0, extract_field_id_from_header(a_buffer[0]));
+
+    let b: i8 = -1;
+    let mut b_buffer = [0; 2];
+    b.serialize_with_header(1, b_buffer.as_mut_slice());
+
+    assert_eq!(1, extract_field_id_from_header(b_buffer[0]));
+
+    let c: u16 = 2;
+
+    let mut c_buffer = [0; 3];
+    c.serialize_with_header(2, c_buffer.as_mut_slice());
+
+    assert_eq!(2, extract_field_id_from_header(c_buffer[0]));
+
+    let d: i16 = -2;
+
+    let mut d_buffer = [0; 3];
+    d.serialize_with_header(3, d_buffer.as_mut_slice());
+
+    assert_eq!(3, extract_field_id_from_header(d_buffer[0]));
+
+    let e: u32 = 3;
+
+    let mut e_buffer = [0; 5];
+    e.serialize_with_header(4, e_buffer.as_mut_slice());
+
+    assert_eq!(4, extract_field_id_from_header(e_buffer[0]));
+
+    let f: i32 = -3;
+
+    let mut f_buffer = [0; 5];
+    f.serialize_with_header(5, f_buffer.as_mut_slice());
+
+    assert_eq!(5, extract_field_id_from_header(f_buffer[0]));
+
+    let g: u64 = 4;
+
+    let mut g_buffer = [0; 9];
+    g.serialize_with_header(6, g_buffer.as_mut_slice());
+
+    assert_eq!(6, extract_field_id_from_header(g_buffer[0]));
+
+    let h: i64 = -4;
+
+    let mut h_buffer = [0; 9];
+    h.serialize_with_header(7, h_buffer.as_mut_slice());
+
+    assert_eq!(7, extract_field_id_from_header(h_buffer[0]));
+}
---
+++ test-suite/src/test/roundtrip.rs
@@ -0,0 +1,127 @@
+
+#[test]
+fn test_base_types_roundtrip() {
+    use prieto_buffers::PrietoBuffersSerde;
+
+    //Add one of every base type
+    #[derive(PrietoBuffersSerde, PartialEq, Debug)]
+    struct RoundtripTestStruct {
+        a: u8,
+        b: i8,
+        c: u16,
+        d: i16,
+        e: u32,
+        f: i32,
+        g: u64,
+        h: i64,
+        i: bool,
+    }
+
+    let a = RoundtripTestStruct {
+        a: 1,
+        b: -2,
+        c: 3,
+        d: -4,
+        e: 5,
+        f: -6,
+        g: 7,
+        h: -8,
+        i: true,
+    };
+
+    //Init with default values
+    let mut b = RoundtripTestStruct {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0,
+        f: 0,
+        g: 0,
+        h: 0,
+        i: false,
+    };
+
+    let mut output = Vec::new();
+
+    let size = a.get_size();
+
+    output.resize(size as usize, 0);
+
+    a.serialize(output.as_mut_slice());
+
+    b.deserialize(output.as_slice());
+
+    assert_eq!(a, b);
+}
+
+#[test]
+fn test_nested_roundtrip() {
+    use prieto_buffers::PrietoBuffersSerde;
+
+    #[derive(PrietoBuffersSerde, PartialEq, Debug)]
+    struct InnerInnerTestStruct {
+        a: u64,
+        b: i64,
+    }
+
+    #[derive(PrietoBuffersSerde, PartialEq, Debug)]
+    struct InnerTestStruct {
+        c: u8,
+        d: InnerInnerTestStruct,
+        e: i8,
+    }
+
+    #[derive(PrietoBuffersSerde, PartialEq, Debug)]
+    struct TestStruct {
+        f: u32,
+        g: InnerTestStruct,
+        h: i16
+    }
+
+    let a = TestStruct {
+        f: 1,
+        g: InnerTestStruct { c: 2, d: InnerInnerTestStruct { a: 3, b: 4 }, e: 5 },
+        h: 6
+    };
+
+    //We init with 0 values
+    let mut b = TestStruct {
+        f: 0,
+        g: InnerTestStruct { c: 0, d: InnerInnerTestStruct { a: 0, b: 0 }, e: 0 },
+        h: 0
+    };
+
+    let size = a.get_size();
+
+    let mut output = Vec::new();
+    output.resize(size as usize, 0);
+
+    a.serialize(output.as_mut_slice());
+    b.deserialize(output.as_slice());
+
+    assert_eq!(a, b);
+}
+
+#[test]
+fn test_empty_struct() {
+    use prieto_buffers::PrietoBuffersSerde;
+
+    #[derive(PrietoBuffersSerde, PartialEq, Debug)]
+    struct EmptyStruct {
+
+    }
+
+    let a = EmptyStruct {};
+    let mut b = EmptyStruct {};
+
+    let size = a.get_size();
+
+    let mut output = Vec::new();
+    output.resize(size as usize, 0);
+    
+    a.serialize(output.as_mut_slice());
+    b.deserialize(output.as_slice());
+
+    assert_eq!(a, b);
+}
