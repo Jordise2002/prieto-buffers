fd8b4e8c5194ac7e57175f5c7c555de8b6a94eb7
Jordise2002 <jordisegura2002@gmail.com>
2026-08-19 14:32:15 +0200

    added support for string, char *, vector and arrays

--- prieto-buffers/src/lib.rs
+++ prieto-buffers/src/lib.rs
@@ -1,4 +1,4 @@
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -8,7 +8,9 @@ pub enum FieldType {
     FourBytes = 2,
     EightBytes = 3,
     Struct = 4,
-    None = 5,//Represents an empty field, used for Option<T> when T is None should not be serialized
+    String = 5,
+    Array = 6,
+    None = 7, //Represents an empty field, used for Option<T> when T is None should not be serialized
 }
 
 impl FieldType {
@@ -18,8 +20,10 @@ impl FieldType {
             FieldType::TwoBytes => 2,
             FieldType::FourBytes => 4,
             FieldType::EightBytes => 8,
-            FieldType::Struct => 0,// Struct sizes are dynamic and determined by their fields
-            FieldType::None => 0
+            FieldType::Struct => 0, // Struct sizes are dynamic and determined by their fields
+            FieldType::String => 0, // String sizes are dynamic and determined by their length
+            FieldType::Array => 0,  // Array sizes are dynamic and determined by their length
+            FieldType::None => 0,
         }
     }
 
@@ -30,6 +34,8 @@ impl FieldType {
             2 => Some(FieldType::FourBytes),
             3 => Some(FieldType::EightBytes),
             4 => Some(FieldType::Struct),
+            5 => Some(FieldType::String),
+            6 => Some(FieldType::Array),
             _ => None,
         }
     }
@@ -57,11 +63,10 @@ pub trait PrietoBuffersSerde {
     fn should_serialize(&self) -> bool {
         true
     }
-    
-    fn serialize_with_header(&self, field_id: u8, bytes: &mut [u8])
-    {
+
+    fn serialize_with_header(&self, field_id: u8, bytes: &mut [u8]) {
         bytes[0] = build_field_header(field_id, self.get_type());
-        self.serialize(& mut bytes[1..]);
+        self.serialize(&mut bytes[1..]);
     }
 
     fn deserialize(&mut self, bytes: &[u8]);
@@ -134,7 +139,6 @@ impl PrietoBuffersSerde for u16 {
         bytes[0] = (*self & 0xFF) as u8;
         bytes[1] = ((*self >> 8) & 0xFF) as u8;
     }
-
 
     fn deserialize(&mut self, bytes: &[u8]) {
         *self = (bytes[0] as u16) | ((bytes[1] as u16) << 8);
@@ -322,5 +326,136 @@ impl<T: PrietoBuffersSerde + Default> PrietoBuffersSerde for Option<T> {
         let mut value = T::default();
         value.deserialize(bytes);
         *self = Some(value);
+    }
+}
+
+#[cfg(feature = "std")]
+impl PrietoBuffersSerde for String {
+    fn get_size(&self) -> u32 {
+        4 + self.len() as u32
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        let length = self.len() as u32;
+        length.serialize(bytes);
+        bytes[4..(4 + length as usize)].copy_from_slice(self.as_bytes());
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        let mut length: u32 = 0;
+        length.deserialize(bytes);
+        let str_bytes = &bytes[4..(4 + length as usize)];
+        *self = String::from_utf8(str_bytes.to_vec()).expect("Invalid UTF-8");
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::String
+    }
+}
+
+#[cfg(feature = "std")]
+impl<T: PrietoBuffersSerde + Default> PrietoBuffersSerde for Vec<T> {
+    fn get_size(&self) -> u32 {
+        let mut total_size = 4;
+        for item in self {
+            total_size += item.get_size();
+        }
+        total_size
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        let length = self.len() as u32;
+        length.serialize(bytes);
+        let mut offset = 4;
+        for item in self {
+            item.serialize(&mut bytes[offset..]);
+            offset += item.get_size() as usize;
+        }
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        let mut length: u32 = 0;
+        length.deserialize(bytes);
+        let mut offset = 4;
+        self.clear();
+        for _ in 0..length {
+            let mut item = T::default();
+            item.deserialize(&bytes[offset..]);
+            offset += item.get_size() as usize;
+            self.push(item);
+        }
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::Array
+    }
+}
+
+impl<T: PrietoBuffersSerde + Default, const N: usize> PrietoBuffersSerde for [T; N] {
+    fn get_size(&self) -> u32 {
+        let mut total_size = 0;
+        for item in self {
+            total_size += item.get_size();
+        }
+        total_size
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        let mut offset = 0;
+        for item in self {
+            item.serialize(&mut bytes[offset..]);
+            offset += item.get_size() as usize;
+        }
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        let mut offset = 0;
+        for item in self {
+            item.deserialize(&bytes[offset..]);
+            offset += item.get_size() as usize;
+        }
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::Array
+    }
+}
+
+impl PrietoBuffersSerde for *mut core::ffi::c_char {
+    fn get_size(&self) -> u32 {
+        let mut len = 0;
+        let mut p = *self;
+
+        unsafe {
+            while *p != 0 {
+                len += 1;
+                p = p.add(1);
+            }
+        }
+
+        len
+    }
+
+    fn serialize(&self, bytes: &mut [u8]) {
+        let size = self.get_size();
+        size.serialize(bytes);
+
+        unsafe {
+            let slice = core::slice::from_raw_parts(*self as *const u8, size as usize);
+            bytes[..size as usize].copy_from_slice(slice);
+        }
+    }
+
+    fn deserialize(&mut self, bytes: &[u8]) {
+        let mut size: u32 = 0;
+        size.deserialize(bytes);
+
+        unsafe {
+            self.copy_from(bytes.as_ptr() as *const i8, size as usize);
+        }
+    }
+
+    fn get_type(&self) -> FieldType {
+        FieldType::String
     }
 }
